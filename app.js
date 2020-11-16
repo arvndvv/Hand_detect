fdc296f11f7561eae2de76835395e18973c8e7f6
Aravind V V <arvndvv@gmail.com>
2020-11-16 13:05:03 +0530

    update

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const modelParams = {
-  flipHorizontal: true, // flip e.g for video
-  imageScaleFactor: 0.7, // reduce input image size for gains in speed.
-  maxNumBoxes: 20, // maximum number of boxes to detect
-  iouThreshold: 0.5, // ioU threshold for non-max suppression
-  scoreThreshold: 0.79 // confidence threshold for predictions.
+    flipHorizontal: true, // flip e.g for video
+    imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+    maxNumBoxes: 20, // maximum number of boxes to detect
+    iouThreshold: 0.5, // ioU threshold for non-max suppression
+    scoreThreshold: 0.9 // confidence threshold for predictions.
 };
 
 navigator.getUserMedia =
-  navigator.getUserMedia ||
-  navigator.webkitGetUserMedia ||
-  navigator.mozGetUserMedia ||
-  navigator.msGetUserMedia;
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
 //select everything in my html
 const video = document.querySelector("#video");
 const audio = document.querySelector("#audio");
@@ -18,29 +18,31 @@ const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
 let model;
 handTrack.startVideo(video).then(status => {
-  if (status) {
-    navigator.getUserMedia(
-      { video: {} },
-      stream => {
-        video.srcObject = stream;
-        //setInterval(runDetection, 1000);
-        runDetection();
-      },
-      err => Console.log(err)
-    );
-  }
-});
-function runDetection() {
-  model.detect(video).then(predictions => {
-    console.log(predictions);
-    if (predictions.length > 0) {
-      audio.play();
-      alert("hands down");
+    console.log("video started", status);
+    if (status) {
+
+        navigator.getUserMedia({ video: {} },
+            stream => {
+                video.srcObject = stream;
+                //setInterval(runDetection, 1000);
+                runDetection();
+            },
+            err => Console.log(err)
+        );
     }
-    requestAnimationFrame(runDetection);
-    //model.renderPredictions(predictions, canvas, context, video);
-  });
+});
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        console.log(predictions);
+        if (predictions.length > 0) {
+            audio.play();
+            //alert("hands down");
+        }
+        requestAnimationFrame(runDetection);
+        //model.renderPredictions(predictions, canvas, context, video);
+    });
 }
 handTrack.load(modelParams).then(lmodel => {
-  model = lmodel;
+    model = lmodel;
 });
